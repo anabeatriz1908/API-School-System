af2475d6dd8e1d9c1e22fb78f57f2b9c8f0524a9
Luiz Otávio <luizotaviossilva@hotmail.com>
2025-04-10 17:10:07 -0300

    Fix: limpeza da pasta

--- antigo/app.py
+++
@@ -1,8 +0,0 @@
-import os
-from antigo.config import app
-from alunos.alunos_routes import alunos_blueprint
-
-app.register_blueprint(alunos_blueprint)
-
-if __name__ == '__main__':
-  app.run(host=app.config["HOST"], port = app.config['PORT'],debug=app.config['DEBUG'] )
--- antigo/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from flask import Flask
-
-
-app = Flask(__name__)
-app.config['HOST'] = '0.0.0.0'
-app.config['PORT']=5036
-app.config['DEBUG'] = True
