6028adb8fbc5b6539523669d17b1b7f04736e054
Luiz <luizotaviossilva@hotmail.com>
2025-05-04 16:53:35 -0300

    Fix: Rota GET aluno por id

--- main/Aluno/alunos_controller.py
+++ main/Aluno/alunos_controller.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
-from main.Aluno.alunos_model import *
+from main.Aluno import alunos_model
+from main.Aluno.alunos_model import AlunoNaoEncontrado
 
 from config import db
 
@@ -10,7 +11,7 @@ alunos_blueprint = Blueprint('alunos', __name__)
 def cria_alunos():
     dados = request.get_json()
     try:
-        novo_aluno, erro = create_alunos(dados)
+        novo_aluno, erro = alunos_model.create_alunos(dados)
         if erro:
             return jsonify({'erro': erro}), 400
         return jsonify(novo_aluno), 200
@@ -21,7 +22,7 @@ def cria_alunos():
 @alunos_blueprint.route('/alunos', methods=['GET'])
 def le_alunos():
     try:
-        alunos,erro = read_alunos()
+        alunos,erro = alunos_model.read_alunos()
         return jsonify(alunos), 200
     except Exception as e:
         return jsonify({'erro': str(e)}), 500
@@ -29,20 +30,22 @@ def le_alunos():
 
 @alunos_blueprint.route('/alunos/<int:id_aluno>', methods =['GET'])
 def le_alunos_id(id_aluno):
-    aluno = read_alunos_id(id_aluno)
-    if aluno:
+    try:
+        aluno = alunos_model.read_alunos_id(id_aluno)
         return jsonify(aluno), 200
-    else:
-        return jsonify({'erro': 'Aluno não encontrado'}), 404 
+    except AlunoNaoEncontrado:
+        return jsonify({'erro': 'Aluno não encontrado'}), 404
+    except Exception as e:
+        return jsonify({'erro': str(e)}), 500
 
 
 @alunos_blueprint.route('/alunos/<int:id_aluno>', methods=['PUT'])
 def atualiza_alunos(id_aluno):
     dados = request.get_json()
     try:
-        atualizado = update_alunos(id_aluno, dados)
+        atualizado = alunos_model.update_alunos(id_aluno, dados)
         if atualizado:
-            return jsonify(read_alunos_id(id_aluno)), 200
+            return jsonify(alunos_model.read_alunos_id(id_aluno)), 200
         else:
             return jsonify({'erro': 'Aluno não encontrado'}), 404
     except Exception as e:
@@ -52,7 +55,7 @@ def atualiza_alunos(id_aluno):
 @alunos_blueprint.route('/alunos/<int:id_aluno>', methods = ['DELETE'])
 def deleta_aluno(id_aluno):
     try:
-        deletado = delete_aluno(id_aluno)
+        deletado = alunos_model.delete_aluno(id_aluno)
         if deletado:
             return jsonify({'mensagem': 'Aluno deletado com sucesso'}), 200
         else:
@@ -64,7 +67,7 @@ def deleta_aluno(id_aluno):
 @alunos_blueprint.route('/alunos', methods = ['DELETE'])
 def deleta_alunos():
     try:
-        deletado, erro = delete_alunos()
+        deletado, erro = alunos_model.delete_alunos()
         if erro:
             return jsonify({'erro': erro}), 400
         return jsonify({'mensagem': 'Todos os alunos foram deletados com sucesso'}), 200
