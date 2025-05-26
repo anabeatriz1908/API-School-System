dc148cab2c8e99d35ec016b89a17410b4eb56df4
Luiz <luizotaviossilva@hotmail.com>
2025-05-26 18:04:48 -0300

    Fix: Trocando o BD que iria expirar

--- config.py
+++ config.py
@@ -7,7 +7,11 @@ app.config['HOST'] = '0.0.0.0'
 app.config['PORT'] = 5036
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ('postgresql://escoladb_kms8_user:GYmK37zARPPGAjheqS4T8EHxFXbLsAyj@dpg-d092nrh5pdvs73a239og-a.oregon-postgres.render.com/escoladb_kms8')
+app.config['SQLALCHEMY_DATABASE_URI'] = ('postgresql://db_school_vgbu_user:BWZqvsho9UlzTjPXFUiA3Z0HoxMsO0Fu@dpg-d0qd9rbuibrs73eh4880-a.oregon-postgres.render.com/db_school_vgbu')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+
+
+# antigo postgresql://escoladb_kms8_user:GYmK37zARPPGAjheqS4T8EHxFXbLsAyj@dpg-d092nrh5pdvs73a239og-a.oregon-postgres.render.com/escoladb_kms8
